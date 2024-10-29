1cfff2c35da8c631f6b7033bd537b4f583149ef3
diowgoschool <diogo.vieira.santos@escola.pr.gov.br>
2024-10-29 10:50:32 -0300

    Implementing new table and page design

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const button = buttons[0];
 const bodies = document.querySelectorAll("tbody");
 const body = bodies[0];
 
+const heads = document.querySelectorAll("thead");
+const head = heads[0];
+
 const tables = document.querySelectorAll("table");
 const table = tables[0];
 
@@ -14,13 +17,26 @@ const inputQuestoes = inputs[1];
 console.log(table);
 
 function construirTabela(nota, questoes) {
+
+    head.innerHTML = "";
     body.innerHTML = "";
+
+    const headAcertos = document.createElement("th");
+    headAcertos.setAttribute("scope", "col");
+    headAcertos.innerHTML = "Acertos";
+    const headNota = document.createElement("th");
+    headNota.setAttribute("scope", "col");
+    headNota.innerHTML = "Nota";
+
+    head.appendChild(headAcertos);
+    head.appendChild(headNota);
+
     for(let i = 0; i <= questoes; i++) {
 
         let valorNota = 10*i/questoes * nota / 10;
         valorNota = valorNota.toFixed(1);
 
-        let rowAcertos = document.createElement("th");
+        let rowAcertos = document.createElement("td");
         rowAcertos.setAttribute("scope", "row");
         rowAcertos.innerText = i.toString();
 
